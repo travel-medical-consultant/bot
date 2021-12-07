0cbfda6d46bfca9832d48c1df48281c52e345dfe
Sushil Awale <ssushil.awale@gmail.com>
2021-12-07 18:49:17 +0100

    First UI changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bot
 Travel Medical Consultant Bot powered by Rasa and GCE
+
+Access the bots at:
+
+- [Here](https://travel-medical-consultant.github.io/bot/639ca6172daf2f1acf5ed7a363fd232b.html)
+- and, [Here](https://travel-medical-consultant.github.io/bot/934cb2ec8182f9d563020e21c052d428.html)
---
+++ public/js/custom.js
@@ -0,0 +1,30 @@
+
+async function showloader() {
+    document.getElementById('loader').classList.remove('hidden');
+}
+
+async function hideloader() {
+    document.getElementById('loader').classList.add('hidden');
+}
+
+function loop(time) {
+    const start = Date.now()
+    while (Date.now() - start < time);
+}
+
+async function wait(time) {
+    showloader().then(
+        loop(wait)
+    ).then(
+        hideloader()
+    );
+}
+
+async function hide(node) {
+    node.parentNode.parentNode.style.display = 'none';
+}
+
+function next(node, load){
+    node.parentNode.parentNode.style.display = 'none';
+    document.getElementById(load).classList.remove('hidden');
+};
